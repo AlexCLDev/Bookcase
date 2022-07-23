982e89d77886ff4304deabd9688029ee71d6741c
Alexander <Alexandercoronado027@gmail.com>
2022-07-23 18:41:48 -0500

    Layout for pages

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,8 +6,29 @@ import { Fragment } from 'react'
 
 export const Home = () => {
   return (
-    <div className='inicio'>
-      <div className='center'/>
+    <div className='home'>
+      <section className='content'>
+        <div className='button'></div>
+        <div className='button'></div>
+        <div className='button  '></div>
+      </section>
+
+      <section className='menu1'>
+        <div className='items'>
+          <p>Hola</p>
+        </div>
+        <div className='items'></div>
+        <div className='items'></div>
+        <div className='items'></div>
+      </section>
+
+      <section className='menu2'>
+      <div className='items'></div>
+        <div className='items'></div>
+        <div className='items'></div>
+        <div className='items'></div>
+        <div className='items'></div>
+      </section>
     </div>
   )
 }
