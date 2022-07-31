ff3b62d83cdc9a5656d110ed1c9b732e07786b7d
Alexander <Alexandercoronado027@gmail.com>
2022-07-31 09:35:14 -0500

    Commit with WorkSpace working

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,34 +1,106 @@
-import React from 'react'
-import { Footer } from '../Layout/Footer'
-import { Header } from '../Layout/Header'
-import './index.scss'
-import { Fragment } from 'react'
+import React from "react";
+import { Footer } from "../Layout/Footer";
+import { Header } from "../Layout/Header";
+import "./index.scss";
+import { Fragment } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faFileAlt,
+  faFileImport,
+  faMusic,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 export const Home = () => {
   return (
-    <div className='home'>
-      <section className='content'>
-        <div className='button'></div>
-        <div className='button'></div>
-        <div className='button  '></div>
-      </section>
-
-      <section className='menu1'>
-        <div className='items'>
-          <p>Hola</p>
+    <div className="home">
+      <section className="contenth">
+        <div className="button">
+          <a>
+            <FontAwesomeIcon icon={faPlus} color="#4d4d4e" />
+            <strong>New Project</strong>
+          </a>
         </div>
-        <div className='items'></div>
-        <div className='items'></div>
-        <div className='items'></div>
+        <div className="button">
+          <a>
+            <FontAwesomeIcon icon={faFileAlt} color="#4d4d4e" />
+            <strong>Open File</strong>
+          </a>
+        </div>
+        <div className="button">
+          <a>
+            <FontAwesomeIcon icon={faFileImport} color="#4d4d4e" />
+            <strong>Import</strong>
+          </a>
+        </div>
+      </section>
+      <h1>Recent Files</h1>
+      <section className="contenth">
+        <div className="items">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </div>
+        <div className="items">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </div>
+        <div className="items">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </div>
+        <div className="items">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </div>
       </section>
 
-      <section className='menu2'>
-      <div className='items'></div>
-        <div className='items'></div>
-        <div className='items'></div>
-        <div className='items'></div>
-        <div className='items'></div>
+      <h1>Library</h1>
+      <section className="contenth">
+        <article className="items2">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </article>
+        <article className="items2">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </article>
+        <article className="items2">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </article>
+        <article className="items2">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </article>
+        <article className="items2">
+          <a>
+            <FontAwesomeIcon icon={faMusic} color="#4d4d4e" />
+          </a>
+          <p>Note</p>
+        </article>
       </section>
     </div>
-  )
-}
+  );
+};
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -4,127 +4,127 @@ import './index.scss'
 import register from '../../Media/recorder.svg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-    faEnvelope,
-    faLock,
-    faUser
-  } from '@fortawesome/free-solid-svg-icons'
+  faEnvelope,
+  faLock,
+  faUser
+} from '@fortawesome/free-solid-svg-icons'
 
 import {
-    faFacebookF,
-    faGoogle,
-    faTwitter,
-    faLinkedinIn,
-  } from '@fortawesome/free-brands-svg-icons'
+  faFacebookF,
+  faGoogle,
+  faTwitter,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons'
 
 export const Login = () => {
 
-    useEffect(() =>{
-        const sign_in_btn = document.querySelector("#sign-in-btn");
-        const sign_up_btn = document.querySelector("#sign-up-btn");
-        const container = document.querySelector(".container");
-        
-        sign_up_btn.addEventListener("click", () => {
-            container.classList.add("sign-up-mode");
-            });
+  useEffect(() => {
+    const sign_in_btn = document.querySelector("#sign-in-btn");
+    const sign_up_btn = document.querySelector("#sign-up-btn");
+    const container = document.querySelector(".container");
 
-            sign_in_btn.addEventListener("click", () => {
-            container.classList.remove("sign-up-mode");
-            });
-    }, [])
+    sign_up_btn.addEventListener("click", () => {
+      container.classList.add("sign-up-mode");
+    });
+
+    sign_in_btn.addEventListener("click", () => {
+      container.classList.remove("sign-up-mode");
+    });
+  }, [])
 
 
   return (
     <div>
-        <div className="container">
-      <div className="forms-container">
-        <div className="signin-signup">
-          <form action="#" className="sign-in-form">
-            <h2 className="title">Sign in</h2>
-            <div className="input-field">
-              <i className="fas fa-user"><FontAwesomeIcon icon={faUser}/></i>
-              <input type="text" placeholder="Username" />
+      <div className="container">
+        <div className="forms-container">
+          <div className="signin-signup">
+            <form action="#" className="sign-in-form">
+              <h2 className="title">Sign in</h2>
+              <div className="input-field">
+                <i className="fas fa-user"><FontAwesomeIcon icon={faUser} /></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"><FontAwesomeIcon icon={faLock} /></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" value="Login" className="btn solid" />
+              <p className="social-text">O inicia con tus redes sociales</p>
+              <div className="social-media">
+                <a href="/" className="social-icon">
+                  <i className="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF} /></i>
+                </a>
+                <a href="/" className="social-icon">
+                  <i className="fab fa-twitter"><FontAwesomeIcon icon={faTwitter} /></i>
+                </a>
+                <a href="/" className="social-icon">
+                  <i className="fab fa-google"><FontAwesomeIcon icon={faGoogle} /></i>
+                </a>
+                <a href="/" className="social-icon">
+                  <i className="fab fa-linkedin-in"><FontAwesomeIcon icon={faLinkedinIn} /></i>
+                </a>
+              </div>
+            </form>
+            <form action="/" className="sign-up-form">
+              <h2 className="title">Sign up</h2>
+              <div className="input-field">
+                <i className="fas fa-user"><FontAwesomeIcon icon={faUser} /></i>
+                <input type="text" placeholder="Username" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-envelope"><FontAwesomeIcon icon={faEnvelope} /></i>
+                <input type="email" placeholder="Email" />
+              </div>
+              <div className="input-field">
+                <i className="fas fa-lock"><FontAwesomeIcon icon={faLock} /></i>
+                <input type="password" placeholder="Password" />
+              </div>
+              <input type="submit" className="btn" value="Sign up" />
+              <p class="social-text">Or Sign up with social platforms</p>
+              <div className="social-media">
+                <a href="/" className="social-icon">
+                  <i className="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF} /></i>
+                </a>
+                <a href="/" className="social-icon">
+                  <i className="fab fa-twitter"><FontAwesomeIcon icon={faTwitter} /></i>
+                </a>
+                <a href="/" className="social-icon">
+                  <i className="fab fa-google"><FontAwesomeIcon icon={faGoogle} /></i>
+                </a>
+                <a href="/" className="social-icon">
+                  <i className="fab fa-linkedin-in"><FontAwesomeIcon icon={faLinkedinIn} /></i>
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panels-container">
+          <div className="panel left-panel">
+            <div className="content">
+              <h3>Nuevo por aqui?</h3>
+              <p>
+                Registrate para que puedas guardar tus documentos, audios y mucho mas.
+              </p>
+              <button className="btn transparent" id="sign-up-btn">
+                Sign up
+              </button>
             </div>
-            <div className="input-field">
-              <i className="fas fa-lock"><FontAwesomeIcon icon={faLock}/></i>
-              <input type="password" placeholder="Password" />
+            <img src={logo} className="image" alt="" />
+          </div>
+          <div className="panel right-panel">
+            <div className="content">
+              <h3>Ya eres uno de nosotros?</h3>
+              <p>
+                Inicia sesion ahora para que puedas acceder a tus documentos y audios guardados.
+              </p>
+              <button className="btn transparent" id="sign-in-btn">
+                Sign in
+              </button>
             </div>
-            <input type="submit" value="Login" className="btn solid" />
-            <p className="social-text">O inicia con tus redes sociales</p>
-            <div className="social-media">
-              <a href="/" className="social-icon">
-                <i className="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF}/></i>
-              </a>
-              <a href="/" className="social-icon">
-                <i className="fab fa-twitter"><FontAwesomeIcon icon={faTwitter}/></i>
-              </a>
-              <a href="/" className="social-icon">
-                <i className="fab fa-google"><FontAwesomeIcon icon={faGoogle}/></i>
-              </a>
-              <a href="/" className="social-icon">
-                <i className="fab fa-linkedin-in"><FontAwesomeIcon icon={faLinkedinIn}/></i>
-              </a>
-            </div>
-          </form>
-          <form action="/" className="sign-up-form">
-            <h2 className="title">Sign up</h2>
-            <div className="input-field">
-              <i className="fas fa-user"><FontAwesomeIcon icon={faUser}/></i>
-              <input type="text" placeholder="Username" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-envelope"><FontAwesomeIcon icon={faEnvelope}/></i>
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="input-field">
-              <i className="fas fa-lock"><FontAwesomeIcon icon={faLock}/></i>
-              <input type="password" placeholder="Password" />
-            </div>
-            <input type="submit" className="btn" value="Sign up" />
-            <p class="social-text">Or Sign up with social platforms</p>
-            <div className="social-media">
-              <a href="/" className="social-icon">
-                <i className="fab fa-facebook-f"><FontAwesomeIcon icon={faFacebookF}/></i>
-              </a>
-              <a href="/" className="social-icon">
-                <i className="fab fa-twitter"><FontAwesomeIcon icon={faTwitter}/></i>
-              </a>
-              <a href="/" className="social-icon">
-                <i className="fab fa-google"><FontAwesomeIcon icon={faGoogle}/></i>
-              </a>
-              <a href="/" className="social-icon">
-                <i className="fab fa-linkedin-in"><FontAwesomeIcon icon={faLinkedinIn}/></i>
-              </a>
-            </div>
-          </form>
+            <img src={register} class="image" alt="" />
+          </div>
         </div>
       </div>
-      <div className="panels-container">
-        <div className="panel left-panel">
-          <div className="content">
-            <h3>Nuevo por aqui?</h3>
-            <p>
-              Registrate para que puedas guardar tus documentos, audios y mucho mas.
-            </p>
-            <button className="btn transparent" id="sign-up-btn">
-              Sign up
-            </button>
-          </div>
-          <img src={logo} className="image" alt="" />
-        </div>
-        <div className="panel right-panel">
-          <div className="content">
-            <h3>Ya eres uno de nosotros?</h3>
-            <p>
-              Inicia sesion ahora para que puedas acceder a tus documentos y audios guardados.
-            </p>
-            <button className="btn transparent" id="sign-in-btn">
-              Sign in
-            </button>
-          </div>
-          <img src={register} class="image" alt="" />
-        </div>
-      </div>
-    </div>
     </div>
   )
 }
